5eb36db81b5cbba9a153f257078a92194963e6d9
raghavhiteshkumar-art <raghavhiteshkumar@gmail.com>
2026-04-20 09:55:51 +0000

    code config done

---
+++ 01.basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountID = 122456
+let accountEmail = "Hitesh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Noida"
+let accountState;
+
+
+//accountID = 2 // not allowed
+accountEmail = "aka@gmail.com"
+accountPassword = "212121"
+accountCity = "Delhi"
+console.log(accountID);
+
+/* prefer not to use var
+because fo issue in block scope and functional scope*/
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
+
